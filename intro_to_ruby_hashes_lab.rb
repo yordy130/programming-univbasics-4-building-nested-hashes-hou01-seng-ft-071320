5145827ba97a62eda76f3583168852c4ed31a611
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-06-25 16:29:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,10 @@ def monopoly_with_third_tier
         four_pieces_owned: 200
         },
         names: {
-          
+          reading_railroad: {},
+          pennsylvania_railroad: {},
+          b_and_o_railroad: {},
+          shortline_railroad: {}
         }
     
     }
